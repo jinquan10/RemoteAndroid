daffe24e79b8f100b7c02a8dae21aae0a1fb6e45
John Zhuang <Jinquan.J.Zhuang.-ND@espn.com>
2014-09-17 17:53:35 -0700

    Enabling WebSocket

--- remoid-webapp/src/main/java/com/miw/remoid/config/AppConfig.java
+++ remoid-webapp/src/main/java/com/miw/remoid/config/AppConfig.java
@@ -14,6 +14,7 @@ import com.miw.remoid.util.EnvironmentPropertyPlaceholderConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableWebSocket
 @ComponentScan(basePackages = "com.miw.remoid")
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
